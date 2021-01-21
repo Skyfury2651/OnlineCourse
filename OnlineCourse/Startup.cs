98e258fa176c543c24640c655d1e5aa1ab38e0bb
skyfury2651 <skyfury2651@gmail.com>
2021-01-21 20:21:38 +0700

    first commit

---
+++ OnlineCourse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestMail.Startup))]
+namespace TestMail
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
