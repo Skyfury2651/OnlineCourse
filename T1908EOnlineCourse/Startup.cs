81950c6c94ed60183c4d9fa3e65b0310c81c1002
skyfury2651 <skyfury2651@gmail.com>
2021-01-24 14:04:08 +0700

    base authorize , email verify

---
+++ T1908EOnlineCourse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(T1908EOnlineCourse.Startup))]
+namespace T1908EOnlineCourse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
