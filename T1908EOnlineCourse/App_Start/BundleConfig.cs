c90b7db20f722159377c3001eb533e53dfca8942
skyfury2651 <skyfury2651@gmail.com>
2021-03-03 22:15:54 +0700

    update view

--- T1908EOnlineCourse/App_Start/BundleConfig.cs
+++ T1908EOnlineCourse/App_Start/BundleConfig.cs
@@ -25,6 +25,30 @@ namespace T1908EOnlineCourse
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/quirk-css").Include(
+                        "~/Content/quirk/Hover/hover.css",
+                        "~/Content/quirk/fontawesome/css/font-awesome.css",
+                        "~/Content/quirk/weather-icons/css/weather-icons.css",
+                        "~/Content/quirk/ionicons/css/ionicons.css",
+                        "~/Content/quirk/jquery-toggles/toggles-full.css",
+                        "~/Content/quirk/morrisjs/morris.css",
+                        "~/Content/quirk/quirk.css"
+                        ));
+            bundles.Add(new ScriptBundle("~/bundles/quirk-js").Include(
+                      "~/Content/quirk/modernizr/modernizr.js",
+                      "~/Content/quirk/jquery/jquery.js",
+                      "~/Content/quirk/jquery-ui/jquery-ui.js",
+                      "~/Content/quirk/bootstrap/js/bootstrap.js",
+                      "~/Content/quirk/jquery-toggles/toggles.js",
+                      "~/Content/quirk/morrisjs/morris.js",
+                      "~/Content/quirk/raphael/raphael.js",
+                      "~/Content/quirk/flot/jquery.flot.js",
+                      "~/Content/quirk/flot/jquery.flot.resize.js",
+                      "~/Content/quirk/flot-spline/jquery.flot.spline.js",
+                      "~/Content/quirk/jquery-knob/jquery.knob.js",
+                      "~/Content/quirk/quirk.js"
+                      ));
         }
     }
 }
