e20eb6566d4724caa3576e9916a9397c688384b2
NTDuong2405 <ntduonga15568@gmail.com>
2021-03-04 06:27:54 -0800

    Update UI

--- T1908EOnlineCourse/Controllers/CourseController.cs
+++ T1908EOnlineCourse/Controllers/CourseController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using T1908EOnlineCourse.Models;
 
 namespace T1908EOnlineCourse.Controllers
 {
     public class CourseController : Controller
     {
-        [Authorize(Roles ="CourseManager")]
-        public ActionResult Index()
+        Model2 _db = new Model2();
+        public ActionResult Index(string search_input)
         {
-            return View();
+            var data = _db.Courses.ToList();
+            if (!string.IsNullOrEmpty(search_input))
+            {
+                data = data.Where(x => x.name.ToLower().Contains(search_input.Trim().ToLower())).ToList();
+            }
+            return View(data);
         }
+        //public ActionResult SearchCourse(string search_input)
+        //{
+        //    var model = _db.Courses.ToList();
+        //    if (!string.IsNullOrEmpty(search_input))
+        //    {
+        //        model = model.Where(x => x.name.ToLower().Contains(search_input.Trim().ToLower())).ToList();
+        //    }
+        //    return View(model);
+        //}
     }
+    
 }
--- T1908EOnlineCourse/Migrations/Configuration.cs
+++ T1908EOnlineCourse/Migrations/Configuration.cs
@@ -20,27 +20,43 @@ namespace T1908EOnlineCourse.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
             context.Categories.AddOrUpdate(c => c.id,
-              new Models.Category { id = 1, name = @"Front-End" , image = @"qqqq"},
+              new Models.Category { id = 1, name = @"Front-End", image = @"qqqq" },
               new Models.Category { id = 2, name = @"Back-End", image = @"string" },
               new Models.Category { id = 3, name = @"Dev-Ops", image = @"string" },
               new Models.Category { id = 4, name = @"Bussiness-Analystic", image = @"string" },
               new Models.Category { id = 5, name = @"Project Manage", image = @"string" }
               );
-            //context.Courses.AddOrUpdate(c => c.id,
+
+            //context.Courses.AddOrUpdate(x => x.id,
             //new Models.Course
             //{
             //    id = 1,
             //    category_id = 1,
-            //    name = @"HTML",
-            //    description = @"So what is HTML? HTML is a markup language that defines the structure of your content. HTML consists of a series of elements, which you use to enclose, or wrap, different parts of the content to make it appear a certain way, or act a certain way. The enclosing tags can make a word or image hyperlink to somewhere else, can italicize words, can make the font bigger or smaller, and so on.  For example, take the following line of content:
-
-            //My cat is very grumpy
-            //If we wanted the line to stand by itself, we could specify that it is a paragraph by enclosing it in paragraph tags:
-
-            //<p>My cat is very grumpy</p>",
-            //    price = 28,
+            //    description = "HTML (Hypertext Markup Language) is the code that is used to structure a web page and its content. For example, content could be structured within a set of paragraphs, a list of bulleted points, or using images and data tables. As the title suggests, this article will give you a basic understanding of HTML and its functions.",
+            //    name = "Introduction to HTML",
             //    status = 1,
-            //    image = "string"
+            //    price = 10,
+            //    image = "https://www.duomly.com/course/courseimg/courses/courseID12/course-12-html---css-basics.jpg"
+            //}
+            //new Models.Course
+            //{
+            //    id = 2,
+            //    category_id = 1,
+            //    description = "We've looked at a lot of text so far in this course, but the web would be really boring only using text. Let's start looking at how to make the web come alive with more interesting content! This module explores how to use HTML to include multimedia in your web pages, including the different ways that images can be included, and how to embed video, audio, and even entire webpages.",
+            //    name = "Multimedia and embedding",
+            //    status = 1,
+            //    price = 20,
+            //    image = "https://1.bp.blogspot.com/-bRc_6ua8YhA/XMWuToHKzkI/AAAAAAAAN3k/ZasbNr9q2FEmgSt957ypx9SnGbjgjX92QCLcBGAs/w1200-h630-p-k-no-nu/Build%2BResponsive%2BReal%2BWorld%2BWebsites%2Bwith%2BHTML5%2Band%2BCSS3%2BUdemy%2Bcourse.png"
+            //},
+            //new Models.Course
+            //{
+            //    id = 3,
+            //    category_id = 1,
+            //    description = "A very common task in HTML is structuring tabular data, and it has a number of elements and attributes for just this purpose. Coupled with a little CSS for styling, HTML makes it easy to display tables of information on the web such as your school lesson plan, the timetable at your local swimming pool, or statistics about your favorite dinosaurs or football team. This module takes you through all you need to know about structuring tabular data using HTML.",
+            //    name = "HTML tables",
+            //    status = 1,
+            //    price = 30,
+            //    image = "https://repository-images.githubusercontent.com/134285701/635de980-586d-11ea-9220-1a3211239c30"
             //}
             //);
             context.Courses.AddOrUpdate(x => x.id,
@@ -48,33 +64,34 @@ namespace T1908EOnlineCourse.Migrations
                 {
                     id = 1,
                     category_id = 1,
-                    description = "text",
-                    name = "test",
+                    description = "We've looked at a lot of text so far in this and how to embed video, audio, and even entire webpages.",
+                    name = "Multimedia and embedding",
                     status = 1,
-                    price = 1000,
+                    price = 20,
+                    image = "https://1.bp.blogspot.com/-bRc_6ua8YhA/XMWuToHKzkI/AAAAAAAAN3k/ZasbNr9q2FEmgSt957ypx9SnGbjgjX92QCLcBGAs/w1200-h630-p-k-no-nu/Build%2BResponsive%2BReal%2BWorld%2BWebsites%2Bwith%2BHTML5%2Band%2BCSS3%2BUdemy%2Bcourse.png"
+                },
+                new Models.Course
+                {
+                    id = 2,
+                    name = "Introduction to HTML",
+                    category_id = 1,
+                    description = "HTML (Hypertext Markup Language) is the code that is used to learn.",
+                    status = 1,
+                    price = 10,
                     image = "https://www.duomly.com/course/courseimg/courses/courseID12/course-12-html---css-basics.jpg"
                 },
-            new Models.Course
-            {
-                id = 2,
-                category_id = 1,
-                description = "text",
-                name = "test",
-                status = 1,
-                price = 1000,
-                image = "222"
-            },
-            new Models.Course
-            {
-                id = 3,
-                category_id = 1,
-                description = "text",
-                name = "test",
-                status = 1,
-                price = 1000,
-                image = "222"
-            }
-            );
+                new Models.Course
+                {
+                    id = 3,
+                    category_id = 1,
+                    description = "A very common task in HTML , or statistics about your favorite dinosaurs or football team. This module takes you through all you need to know about structuring tabular data using HTML.",
+                    name = "HTML tables",
+                    status = 1,
+                    price = 30,
+                    image = "https://repository-images.githubusercontent.com/134285701/635de980-586d-11ea-9220-1a3211239c30"
+                }
+                );
         }
     }
 }
+
