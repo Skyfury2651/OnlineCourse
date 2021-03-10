db37d54b7bb33161f49d96ecf8f8abf8f5284ccc
NTDuong2405 <ntduonga15568@gmail.com>
2021-03-10 20:24:16 +0700

    UI for Instructor

---
+++ T1908EOnlineCourse/Controllers/TeacherController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T1908EOnlineCourse.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult InformationTable()
+        {
+            return View();
+        }
+    }
+}
