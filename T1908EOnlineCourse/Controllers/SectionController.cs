b1bf3efad15986a102ccfe246bf2c9826b29079e
tlhoang1211 <tlhoang1211@gmail.com>
2021-03-12 23:44:07 +0700

    lecture

---
+++ T1908EOnlineCourse/Controllers/SectionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using T1908EOnlineCourse.Models;
+
+namespace T1908EOnlineCourse.Controllers
+{
+    public class SectionController : Controller
+    {
+        Model2 _db = new Model2();
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Lecture
+        public ActionResult Index()
+        {
+            return null;
+        }
+
+        public ActionResult Lecture(int id)
+        {
+            var data = _db.Sections.Find(id);
+            if (data == null)
+            {
+                return HttpNotFound();
+            }
+            return View(data);
+        }
+    }
+}
